7d5ad8da0f7f0b8b9e9a3db18a42a645d009ea48
tanmayVaish <55585498+tanmayVaish@users.noreply.github.com>
2024-05-07 19:57:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Button} from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant={"contained"} color="secondary">Hello World</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
